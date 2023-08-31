62ae481c06e88df62495b70566d327ea456dd13d
Aaron Melendez <aaronmelendez37@gmail.com>
2023-08-30 23:35:08 -0700

    wip

--- src/client/public/script.js
+++ src/client/public/script.js
@@ -1,13 +1,23 @@
 import axios from "axios";
 import { saveAs } from "file-saver";
+import crypto from "crypto-js";
 
+// todo: find library to accurately convert hex to base64
 document.addEventListener("DOMContentLoaded", function () {
   const loadBtn = document.getElementById("load-btn");
   const textInput = document.getElementById("text-input");
   loadBtn.addEventListener("click", async () => {
     const imageName = textInput.value;
     const image = await axios.get(`http://localhost:3001/images/${imageName}`);
-    const blob = new Blob([image.data], { type: "image/png" });
-    saveAs(blob, `${imageName}`); // figure out why it's in the wrong format and can't be opened
+    const byteArray = crypto.enc.Hex.parse(image.data);
+    const base64String = crypto.enc.Base64.stringify(byteArray);
+    const baseImage = new Image();
+    baseImage.src = "data:image/png;base64," + base64String;
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext("2d");
+    context.drawImage(baseImage, 0, 0);
+    canvas.toBlob(function (blob) {
+      saveAs(blob, imageName);
+    }, "image/png");
   });
 });
