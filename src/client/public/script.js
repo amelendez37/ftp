df4d30f3c4a1eb1469703754ba4d3d10fdb3227c
Aaron Melendez <aaronmelendez37@gmail.com>
2023-12-20 13:10:52 -0800

    add comments

--- src/client/public/script.js
+++ src/client/public/script.js
@@ -1,22 +1,21 @@
 import axios from "axios";
-import { saveAs } from "file-saver";
-import crypto from "crypto-js";
 
 function arrayBufferToBlob(arrayBuffer) {
   return new Blob([arrayBuffer], { type: "image/png" });
 }
 
-// TODO: write comments about what code is doing to solidify understanding
 document.addEventListener("DOMContentLoaded", function () {
   const loadBtn = document.getElementById("load-btn");
   const textInput = document.getElementById("text-input");
 
   loadBtn.addEventListener("click", async () => {
     const imageName = textInput.value;
+    // get image from server. Keep data as an ArrayBuffer
     const image = await axios.get(`http://localhost:3001/images/${imageName}`, {
       responseType: "arraybuffer",
     });
     const blob = arrayBufferToBlob(image.data);
+    // convert blob to object url and programmatically click to download
     const a = document.createElement("a");
     a.href = URL.createObjectURL(blob);
     a.download = imageName;
